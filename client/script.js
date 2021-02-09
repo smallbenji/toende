d61622afdfd3bb1c78ed47044bf4d6787c04d2d0
smallbenji <benjaminkfalch@gmail.com>
2021-02-09 10:51:10 +0100

    coding coding coding

--- client/script.js
+++ client/script.js
@@ -2,15 +2,15 @@ const socket = new WebSocket('ws://localhost:8080');
 
 socket.addEventListener('message', function (event) {
     console.log(event.data);
-    if(event.data.startsWith("id:")){
+    if (event.data.startsWith("id:")) {
         var message = event.data.split(": ");
         console.log(message[1]);
         document.location.href = `toende.html?id=${message[1]}`;
     }
 });
 
-function submit(){
-    if(!document.getElementById('username').value==""){
+function submit() {
+    if (!document.getElementById('username').value == "") {
         socket.send(`user: ${document.getElementById('username').value}`);
     }
 }
